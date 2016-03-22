a11afda0eb097e73dc9847961b87d6dee50afdf5
Rosie Hamilton <enchantrix@gmail.com>
2016-03-22 21:35:16 +0000

    tidy up

--- src/main/main.go
+++ src/main/main.go
@@ -5,28 +5,22 @@ import(
    "net/http"
    "html/template"
    "log"
-//   "io"
-
 )
 func main(){
 
   // this code below makes a file server and serves everything as a file
-  //  fs := http.FileServer(http.Dir(""))
-  //  http.Handle("/", fs)
+  // fs := http.FileServer(http.Dir(""))
+  // http.Handle("/", fs)
 
 
   // serve everything in the css folder as a file
   http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 
   // when navigating to /home it should serve the home page
-
   http.HandleFunc("/", Index)
   http.HandleFunc("/home", Home)
   http.HandleFunc("/scale", Scale)
   http.ListenAndServe(":8080", nil)
-
-
-
 
   }
 
@@ -40,31 +34,29 @@ func main(){
     render(w, "home.html")
   }
 
+// handler for /scale which parses the values submitted from /home
   func Scale(w http.ResponseWriter, r *http.Request){
     r.ParseForm() //r is url.Values which is a map[string][]string
     for key, values := range r.Form {   // range over map
        for _, value := range values {    // range over []string
        fmt.Println(key, value) // print all the keys and values to server console
-    //   io.WriteString(w, key) // display the key in browser
-  //     io.WriteString(w, value) // display the value in browser
- fmt.Fprintf(w, " %s : %s\n", key, value)
-  }
-
-}
-
+       fmt.Fprintf(w, " %s : %s\n", key, value) // and print them in the browser
+       }
+     }
   }
 
   func render(w http.ResponseWriter, tmpl string){
 
     tmpl = fmt.Sprintf("templates/%s", tmpl) // prefix the name passed in with templates/
+    t, err := template.ParseFiles(tmpl) //parse the template
 
-    t, err := template.ParseFiles(tmpl)
-    if err != nil{
-      log.Print("template parsing error: ", err)
+    if err != nil{ // if there is an error
+      log.Print("template parsing error: ", err) // log it
     }
 
-    err = t.Execute(w, "")
-    if err != nil {
-      log.Print("template executing error: ", err)
+    err = t.Execute(w, "") //execute the template
+
+    if err != nil { // if there is an error
+      log.Print("template executing error: ", err) //log it
     }
   }
