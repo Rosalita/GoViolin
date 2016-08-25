88640db346e30ce725f37b7ec4a4102a8ba04647
Rosie Hamilton <enchantrix@gmail.com>
2016-08-25 13:08:27 +0100

    added unit test that asserts expected values are present

--- src/main/scale_test.go
+++ src/main/scale_test.go
@@ -1,21 +1,30 @@
 package main
 
 import "testing"
+import "github.com/stretchr/testify/assert"
+
+type TD struct {
+	Data       string
+  Expected   string
+}
 
 func TestSettingDefaultScaleOptionsNotNil(t *testing.T){
-
+  assert := assert.New(t)
   options1, options2, options3, options4 := setDefaultScaleOptions()
+  assert.NotNil(options1)
+  assert.NotNil(options2)
+  assert.NotNil(options3)
+  assert.NotNil(options4)
+}
 
-  if options1 == nil {
-    t.Error("Test failed, no default options for Scale / Arpeggio")
+func TestSharpToS(t *testing.T){
+  assert := assert.New(t)
+  testData := []TD{
+    TD{"G#", "Gs"},
+    TD{"D#", "Ds"},
+    TD{"Bb", "Bb"},
   }
-  if options2 == nil {
-    t.Error("Test failed, no default options for Major / Minor ")
-  }
-  if options3 == nil {
-    t.Error("Test failed, no default options for Key")
-  }
-  if options4 == nil {
-    t.Error("Test failed, no default options for Octave")
+  for _, test := range testData {
+      assert.Equal(changeSharpToS(test.Data), test.Expected)
   }
 }
